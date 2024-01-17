66755c8b9d2e434b91be473c5a131f6a36f4ea49
Verinikita <80643476+Verinikita@users.noreply.github.com>
2024-01-17 13:11:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,63 @@
-from flask import Flask, render_template, request, send_from_directory
-import os
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
 from zipfile import ZipFile
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './downlo'
 
-UPLOAD_FOLDER = 'uploads'
-JSON_FOLDER = 'archivos_json'
-PDB_FOLDER = 'archivos_pdb'
-# Asegúrate de que las carpetas existan o créalas automáticamente
-for folder in [UPLOAD_FOLDER, JSON_FOLDER, PDB_FOLDER]:
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'zip'}
 
-def aplicar_funcion(zip_path):
-    # Tu lógica para aplicar la función a los archivos ZIP
-    # Aquí estoy simulando la extracción de archivos json y pdb
-    with ZipFile(zip_path, 'r') as fz:
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def AlphaFoldXploR_read(zfile):
+    with ZipFile(zfile, 'r') as fz:
         for zip_info in fz.infolist():
-            if zip_info.filename.endswith(".json"):
+            if zip_info.filename[-1] == '/':
+                continue
+            tab = os.path.basename(zip_info.filename)
+            if tab.endswith(".json"):
                 zip_info.filename = os.path.basename(zip_info.filename)
-                fz.extract(zip_info, JSON_FOLDER)
-            elif zip_info.filename.endswith(".pdb"):
+                lista1 = fz.extract(zip_info, "archivos_json")
+
+    with ZipFile(zfile, 'r') as fz:
+        for zip_info in fz.infolist():
+            if zip_info.filename[-1] == '/':
+                continue
+            tab = os.path.basename(zip_info.filename)
+            if tab.endswith(".pdb"):
                 zip_info.filename = os.path.basename(zip_info.filename)
-                fz.extract(zip_info, PDB_FOLDER)
+                lista2 = fz.extract(zip_info, "archivos_pdb")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def upload_file():
+    return render_template('products.html')
 
-@app.route('/process', methods=['POST'])
-def process():
-    if 'file' not in request.files:
-        return 'Error: No se ha proporcionado ningún archivo'
+@app.route("/upload", methods=['POST'])
+def uploadAndApply():
+    if 'zipFile' not in request.files:
+        flash('No se ha proporcionado ningún archivo')
+        return redirect(request.url)
 
-    file = request.files['file']
+    file = request.files['zipFile']
 
     if file.filename == '':
-        return 'Error: No se ha seleccionado ningún archivo'
+        flash('No se ha seleccionado ningún archivo')
+        return redirect(request.url)
 
-    if file and file.filename.endswith(".zip"):
-        zip_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(zip_path)
-        aplicar_funcion(zip_path)
-        return 'Función aplicada con éxito'
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        # Llama a AlphaFoldXploR_read con el nombre del archivo
+        AlphaFoldXploR_read(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        flash('Archivo subido exitosamente')
+        return redirect(url_for('upload_file'))
 
-    return 'Error: El archivo debe tener extensión .zip'
-
-@app.route('/download/<folder_name>')
-def download_folder(folder_name):
-    zip_filename = f"{folder_name}.zip"
-    zip_path = os.path.join(app.config['UPLOAD_FOLDER'], zip_filename)
-
-    with ZipFile(zip_path, 'w') as zipf:
-        folder_path = os.path.join(app.config['UPLOAD_FOLDER'], folder_name)
-        for root, _, files in os.walk(folder_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, os.path.relpath(file_path, folder_path))
-
-    return send_from_directory(app.config['UPLOAD_FOLDER'], zip_filename, as_attachment=True)
+    flash('El tipo de archivo no está permitido')
+    return redirect(request.url)
 
 if __name__ == '__main__':
     app.run(debug=True)
