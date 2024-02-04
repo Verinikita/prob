ead305d6529a18190a8d935f7d7f090b90706786
Verinikita <80643476+Verinikita@users.noreply.github.com>
2024-02-04 00:39:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def AlphaFoldXploR_read(zfile):
+    lista1 = []
+    lista2 = []
+    
     with ZipFile(zfile, 'r') as fz:
         for zip_info in fz.infolist():
             if zip_info.filename[-1] == '/':
@@ -19,7 +22,8 @@ def AlphaFoldXploR_read(zfile):
             tab = os.path.basename(zip_info.filename)
             if tab.endswith(".json"):
                 zip_info.filename = os.path.basename(zip_info.filename)
-                lista1 = fz.extract(zip_info, "archivos_json")
+                lista1.append(zip_info.filename)
+                fz.extract(zip_info, "archivos_json")
 
     with ZipFile(zfile, 'r') as fz:
         for zip_info in fz.infolist():
@@ -28,11 +32,14 @@ def AlphaFoldXploR_read(zfile):
             tab = os.path.basename(zip_info.filename)
             if tab.endswith(".pdb"):
                 zip_info.filename = os.path.basename(zip_info.filename)
-                lista2 = fz.extract(zip_info, "archivos_pdb")
+                lista2.append(zip_info.filename)
+                fz.extract(zip_info, "archivos_pdb")
+    
+    return lista1, lista2
 
 @app.route("/")
 def upload_file():
-    return render_template('products.html')
+    return render_template('index.html')
 
 @app.route("/upload", methods=['POST'])
 def uploadAndApply():
@@ -51,10 +58,10 @@ def uploadAndApply():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         
         # Llama a AlphaFoldXploR_read con el nombre del archivo
-        AlphaFoldXploR_read(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        lista1, lista2 = AlphaFoldXploR_read(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         
         flash('Archivo subido exitosamente')
-        return redirect(url_for('upload_file'))
+        return render_template('index.html', lista1=lista1, lista2=lista2)
 
     flash('El tipo de archivo no está permitido')
     return redirect(request.url)
